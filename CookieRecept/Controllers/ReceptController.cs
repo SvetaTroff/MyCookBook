115547ee8b124839c0f1f72594782ae0d91f8057
sveta <sveta@DESKTOP-KTEF78B>
2017-04-02 23:12:55 +0500

    1 лаба. Верстка

---
+++ CookieRecept/Controllers/ReceptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieRecept.Controllers
+{
+    public class ReceptController : Controller
+    {
+        // GET: Recept
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CookieRecept/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CookieRecept.MvcApplication" Language="C#" %>
